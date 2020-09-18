f12d79a4800d3bbc547610d0763c4dc6ca651763
SenaHuang <sena.huang@104.com.tw>
2020-09-18 17:18:04 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './scss/style.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="app-title">牧場物語 雙子村</h1>
+      <div className="link-group">
+        <div className="btn">魚的介紹</div>
+        <div className="btn">村民簡介</div>
+        <div className="btn">戀愛對象簡介</div>
+        <div className="btn">食譜簡介</div>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
