33dd20a6e305e0d34b716e4f989687f523d8e802
shuanshuan030913 <skypainter59768@gmail.com>
2022-02-24 19:58:40 +0800

    adj layout

---
+++ src/client/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from "react-router-dom";
+import '../client/scss/style.scss';
+import Header from "./layout/Header";
+
+function App() {
+  return (
+      <Router>
+          <div>
+              <Header />
+              <Routes>
+                  <Route path="/" element={<Home />} />
+                  <Route path="/lover" element={<Lover />} />
+                  <Route path="/recipe" element={<Recipe />} />
+                  <Route path="/member" element={<Member />} />
+                  <Route path="/fish" element={<Fish />} />
+              </Routes>
+          </div>
+      </Router>
+  );
+}
+
+function Home() {
+    return <h2>Home</h2>;
+}
+
+function Lover() {
+    return <h2>戀愛對象簡介</h2>;
+}
+
+function Recipe() {
+    return <h2>食譜簡介</h2>;
+}
+
+function Member() {
+    return <h2>村民簡介</h2>;
+}
+
+function Fish() {
+    return <h2>魚的簡介</h2>;
+}
+
+
+export default App;
---
+++ src/client/layout/Header.js
@@ -0,0 +1,139 @@
+import React from 'react';
+import {
+    AppBar, IconButton, Toolbar, Typography, Container, Avatar, Button,
+    Tooltip, MenuItem, Box, Menu
+} from "@mui/material";
+import MenuIcon from '@mui/icons-material/Menu';
+import {Link} from "react-router-dom";
+
+const pages = [
+    {label: '戀愛對象', value: 'lover'},
+    {label: '食譜', value: 'recipe'},
+    {label: '村民', value: 'member'},
+    {label: '魚', value: 'fish'}
+];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+const Header = () => {
+    const [anchorElNav, setAnchorElNav] = React.useState(null);
+    const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    };
+    const handleOpenUserMenu = (event) => {
+        setAnchorElUser(event.currentTarget);
+    };
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    };
+
+    const handleCloseUserMenu = () => {
+        setAnchorElUser(null);
+    };
+
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+                    >
+                        牧場小知識！
+                    </Typography>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <Link key={page.value} to={`/${page.value}`} onClick={() => handleCloseNavMenu()}>
+                                    <MenuItem>
+                                        <Typography textAlign="center">{page.label}</Typography>
+                                    </MenuItem>
+                                </Link>
+                            ))}
+                        </Menu>
+                    </Box>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+                    >
+                        牧場小知識！
+                    </Typography>
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                        {pages.map((page) => (
+                            <Link key={page.value} to={`/${page.value}`} onClick={() => handleCloseNavMenu()}>
+                                <Button sx={{ my: 2, color: 'white', display: 'block' }}>
+                                    {page.label}
+                                </Button>
+                            </Link>
+                        ))}
+                    </Box>
+
+                    <Box sx={{ flexGrow: 0 }}>
+                        <Tooltip title="Open settings">
+                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                            </IconButton>
+                        </Tooltip>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            id="menu-appbar"
+                            anchorEl={anchorElUser}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'right',
+                            }}
+                            open={Boolean(anchorElUser)}
+                            onClose={handleCloseUserMenu}
+                        >
+                            {settings.map((setting) => (
+                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                                    <Typography textAlign="center">{setting}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+};
+export default Header;
