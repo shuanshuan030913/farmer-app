a160d7329693149c261080dd3da585918f764420
shuanshuan030913 <skypainter59768@gmail.com>
2022-03-07 20:41:01 +0800

    add Lover 30%

--- src/client/constants.js
+++ src/client/constants.js
@@ -1,10 +1,100 @@
 module.exports = Object.freeze({
     COUNTRIES: [
-        {id: 1, zh: '藍鈴村', jp: 'ブルーベル村'},
-        {id: 2, zh: '此花村', jp: 'このはな村'},
+        {id: 0, zh: '藍鈴村', jp: 'ブルーベル村'},
+        {id: 1, zh: '此花村', jp: 'このはな村'},
+        {id: 2, zh: '其他'},
     ],
-    TABS: [
-        {name: {zh: '亞修', jp: 'アーシュ'}, },
-        {name: {zh: '卡米爾', jp: 'カミル'}, },
+    SEASON: ['春', '夏', '秋', '冬'],
+    Lovers: [
+        [
+            {
+                id: 1,
+                gender: 1,
+                name: {zh: '亞修', jp: 'アーシュ'},
+                birthday: [0, 20],
+                init: -1,
+            },
+            {
+                id: 2,
+                gender: 1,
+                name: {zh: '卡米爾', jp: 'カミル'},
+                birthday: [2, 1],
+                init: -1,
+            },
+            {
+                id: 3,
+                gender: 0,
+                name: {zh: '拉茲貝莉', jp: 'ラズベリー'},
+                birthday: [0, 4],
+                init: -1,
+            },
+            {
+                id: 4,
+                gender: 0,
+                name: {zh: '莉亞', jp: 'リア'},
+                birthday: [1, 23],
+                init: -1,
+            },
+            {
+                id: 5,
+                gender: 0,
+                name: {zh: '艾瑞拉', jp: 'アリエラ'},
+                birthday: [3, 17],
+                init: 1,
+            },
+        ],
+        [
+            {
+                id: 1,
+                gender: 1,
+                name: {zh: '奇利克', jp: 'キリク'},
+                birthday: [1, 13],
+                init: -1,
+            },
+            {
+                id: 2,
+                gender: 1,
+                name: {zh: '千尋', jp: 'チヒロ'},
+                birthday: [3, 5],
+                init: -1,
+            },
+            {
+                id: 3,
+                gender: 0,
+                name: {zh: '娜娜', jp: 'ナナ'},
+                birthday: [0, 27],
+                init: -1,
+            },
+            {
+                id: 4,
+                gender: 0,
+                name: {zh: '莉可麗絲', jp: 'リコリス'},
+                birthday: [3, 27],
+                init: -1,
+            },
+            {
+                id: 5,
+                gender: 0,
+                name: {zh: '迪魯卡', jp: 'ディルカ'},
+                birthday: [1, 12],
+                init: 2,
+            },
+        ],
+        [
+            {
+                id: 1,
+                gender: 0,
+                name: {zh: '賢者大人', jp: '賢者さま'},
+                birthday: [3, 30],
+                init: 3,
+            },
+            {
+                id: 2,
+                gender: 1,
+                name: {zh: '米海爾', jp: 'ミハイル'},
+                birthday: [2, 12],
+                init: 4,
+            },
+        ],
     ]
 });
--- src/client/pages/Lover.js
+++ src/client/pages/Lover.js
@@ -1,13 +1,13 @@
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
 import {map} from 'lodash';
 
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import {COUNTRIES} from '../constants';
+import {COUNTRIES, Lovers, SEASON} from '../constants';
+import {Grid, List, ListItemButton, ListItemText} from "@mui/material";
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -22,7 +22,7 @@ function TabPanel(props) {
         >
             {value === index && (
                 <Box sx={{ p: 3 }}>
-                    <Typography>{children}</Typography>
+                    {children}
                 </Box>
             )}
         </div>
@@ -34,45 +34,102 @@ TabPanel.propTypes = {
     value: PropTypes.number.isRequired,
 };
 
-function a11yProps(index) {
-    return {
-        id: `simple-tab-${index}`,
-        'aria-controls': `simple-tabpanel-${index}`,
-    };
+const LoverInfo = ({data}) => {
+    const {name, birthday, init} = data;
+
+    const setInit = v => {
+        if (v === 4) return '藍鈴村左山道第2張地圖，必須使用彈跳蘑菇，再使用岩石滑道後的右手邊。';
+        if (v === 3) return '第2年後，晴天，20：00後，徒步從藍鈴村左山道進入賢者大人住宅的地圖。';
+        if (v === 2) return '第1年夏後，晴天，AM10：00～PM18：59，徒步從山道進入此花村。';
+        if (v === 1) return '第2年秋後，晴天，22：00後，徒步從山道走到山頂。(選項選第2個)';
+        return '最初登場';
+    }
+    return <div>
+        <div>姓名：{name.zh}({name.jp})</div>
+        <div>生日：{SEASON[birthday[0]]} {birthday[1]} 日</div>
+        <div>登場條件：{setInit(init)}</div>
+        <div>家庭成員：</div>
+        <div>居住地點：</div>
+        <div>喜歡的服裝：</div>
+        <div>最喜歡：</div>
+        <div>喜歡：</div>
+        <div>討厭：</div>
+        <div>最討厭：</div>
+        {/*家庭成員：母親－傑西卡(ジェシカ)、妹妹－雪莉露(シェリル)。*/}
+        {/*居住地點：杰修‧動物屋(ジェシュ・アニマルリー)。*/}
+        {/*喜歡的服裝：工作女裝(ワーキングガール)。*/}
+        {/*最喜歡：ドリア(義式烤飯：平底鍋 + 洋蔥 + 小麥粉 + 米飯 + 牛奶)*/}
+        {/*喜歡：生チョコ系のお菓子(生巧克力的點心)、焼きいも(烤地瓜)、オムライス(蛋包飯)、オムレツ(煎雞蛋卷)、グラタン(奶汁烤菜)、シチュー(炖菜)、牛乳なべ(牛奶鍋)、クリームコロッケ(奶油炸肉餅)、アイスクリーム(冰淇淋)、スイートポテト(甜土豆)、バウムクーヘン(蛋糕卷)、ホットミルク(熱牛奶)、ミルクティー(奶茶)、ココア(可可茶)、カフェオレ(牛奶咖啡)、カプチーノ(卡布奇諾咖啡)、マジックレッド(魔術紅草)。*/}
+        {/*討厭：綠茶、きのこ系(蘑菇系)、豆腐ステーキ(煎豆腐)。*/}
+        {/*最討厭：焼ききのこ(烤蘑菇)。*/}
+    </div>
 }
 
-function BasicTabs() {
+const SelectedListItem = ({country}) => {
+    const [selectedId, setSelectedId] = React.useState(Lovers[country.id][0].id);
+
+    const handleListItemClick = (event, id) => setSelectedId(id);
+
+    useEffect(() => {
+        setSelectedId(Lovers[country.id][0].id);
+    }, [country])
+
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                <Grid item xs={4}>
+                    <List component="nav" aria-label="mailbox folder">
+                        {map(Lovers[country.id], lover => <ListItemButton {...{
+                            key: lover.id,
+                            selected: selectedId === lover.id,
+                            onClick: (event) => handleListItemClick(event, lover.id),
+                        }}>
+                            <ListItemText primary={lover.name.zh} />
+                        </ListItemButton>)}
+                    </List>
+                </Grid>
+                <Grid item xs={8}>
+                    <LoverInfo {...{data: Lovers[country.id][selectedId-1]}} />
+                </Grid>
+            </Grid>
+        </Box>
+    );
+}
+
+function Lover() {
     const [value, setValue] = React.useState(0);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
 
-    return (
+    const a11yProps = (index) => {
+        return {
+            id: `simple-tab-${index}`,
+            'aria-controls': `simple-tabpanel-${index}`,
+        };
+    }
+
+    return <div>
+        <h2>戀愛對象簡介</h2>
         <Box sx={{ width: '100%' }}>
             <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                 <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
-                    {map(COUNTRIES, (c, idx) => <Tab {...{
-                            key: idx,
-                            label: `${c.zh}(${c.jp})`,
-                            ...a11yProps(idx),
-                        }} />)}
+                    {map(COUNTRIES, c => <Tab {...{
+                        key: c.id,
+                        label: `${c.zh}${c.jp ? ' (' + c.jp + ')' : ''}`,
+                        ...a11yProps(c.id),
+                    }} />)}
                 </Tabs>
             </Box>
             {map(COUNTRIES, (c, idx) => <TabPanel {...{
-                    value: value,
-                    index: idx,
+                key: c.id,
+                value: value,
+                index: idx,
             }}>
-                <div>{c.zh}({c.jp})</div>
+                <SelectedListItem {...{ country: c }} />
             </TabPanel>)}
         </Box>
-    );
-}
-
-function Lover() {
-    return <div>
-        <h2>戀愛對象簡介</h2>
-        <BasicTabs />
     </div>;
 }
 
